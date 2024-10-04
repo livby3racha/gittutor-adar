90c7dca972019b7ec52d9bbf3e78d298ed110173
lhmet <lhmet@ufsm.br>
2024-10-04 11:21:51 -0300

    adaptação da funcao original para simplesmente somar 1

--- exemplo-funcao-basica.R
+++ exemplo-funcao-basica.R
@@ -1,8 +1,8 @@
 
 # Funcao para somar o valor 1
-soma_k <- function(x, k) x + k
+soma_1 <- function(x) x + 1
 
 # exemplo 
-soma_k(x = 10, k = 2)
+soma_1(x = 10)
 
-soma_k(x = 1:10, k = 5)
+soma_1(x = 1:10)
