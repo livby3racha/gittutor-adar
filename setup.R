acfa04ffc4e9e967874c698f34837aed30da4780
livby3racha <livia.sousa@acad.ufsm.br>
2025-09-05 11:25:56 -0300

    Atualização das configurações do git para o RStudio, a partir do livro de ADAR. Modificações para uso do git na sala 1035.

--- setup.R
+++ setup.R
@@ -21,11 +21,13 @@ usethis::git_vaccinate()
 
 
 library(usethis) 
-use_git_config(user.name = "lhmet", user.email = "lhmet@ufsm.br")
+use_git_config(user.name = "livby3racha", user.email = "livia.sousa@acad.ufsm.br")
 
 
 # check by running a git situation-report: 
 #   - your user.name and user.email should appear in global Git config 
+
+# verificar configurações do git
 git_sitrep()
 
 
@@ -35,10 +37,11 @@ usethis::create_github_token()
 # salvar token
 gitcreds::gitcreds_set()
 
+pak::pak('usethis')
 
+# token github 1035: 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
 
-
-
+# link de como configurar o git: https://happygitwithr.com/https-pat.html
 
 
 
